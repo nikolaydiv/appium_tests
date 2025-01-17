0d55e6f97cd710b98a4cc1c9bd411cf870b429d5
nikolay divdevilov <nikolaydiv@gmail.com>
2025-01-17 12:49:52 +0500

    initial commit

---
+++ sn_by_image.py
@@ -0,0 +1,81 @@
+# IN PROGRESS
+
+import time
+import unittest
+
+import psutil
+from appium import webdriver
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+import base64
+import os
+import cv2
+
+capabilities = dict(
+    platformName='Android',
+    automationName='uiautomator2',
+    deviceName='Android',
+    language='en',
+    locale='US',
+    appPackage='com.mytona.seekersnotes.android',
+    appActivity='.GameStartActivity'
+)
+
+appium_server_url = 'http://localhost:4723'
+
+
+class TestAppium(unittest.TestCase):
+    driver = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.driver = webdriver.Remote(
+            appium_server_url,
+            options=UiAutomator2Options().load_capabilities(capabilities)
+        )
+
+    # @classmethod
+    # def tearDownClass(cls) -> None:
+    #     if cls.driver:
+    #         cls.driver.quit()
+
+    def save_screenshot(self, filename: str) -> str:
+        screenshot_path = os.path.join(os.getcwd(), filename)
+        self.driver.save_screenshot(screenshot_path)
+        print(f'Saved screenshot: {filename}')
+        return screenshot_path
+
+    def find_and_tap(self, button_filename: str) -> None:
+        screenshot_path = self.save_screenshot("screenshot.png")
+        self.driver.save_screenshot(screenshot_path)
+        screenshot = cv2.imread(screenshot_path)
+        template_path = os.path.join(os.getcwd(), button_filename)
+
+        template = cv2.imread(template_path)
+        result = cv2.matchTemplate(screenshot, template, cv2.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        top_left = max_loc
+        h, w = template.shape[:2]
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+        cv2.rectangle(screenshot, top_left, bottom_right, (0, 255, 0), 2)
+        result_image_path = os.path.join(os.getcwd(), "result.png")
+        cv2.imwrite(result_image_path, screenshot)
+
+        print(f'Результат сохранен в {result_image_path}')
+        if max_val < 6:
+            print(f'Уровень совпадения: {max_val}')
+            raise Exception(f'Кнопка {button_filename} не найдена')
+
+        center_x = top_left[0] + w // 2
+        center_y = top_left[1] + h // 2
+        self.driver.tap([(center_x, center_y)])
+        print(f'Кнопка {button_filename} найдена и нажата по координатам: {center_x}, {center_y}')
+
+    def test_find_and_tap(self) -> None:
+        time.sleep(10)
+        self.find_and_tap('apply_button.PNG')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ sn_by_pixel.py
@@ -0,0 +1,118 @@
+# простой appium тест с запуском SN на 1422 и прохождением начального тутора
+import time
+import unittest
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+
+capabilities = dict(
+    platformName='Android',
+    automationName='uiautomator2',
+    deviceName='Android',
+    language='en',
+    locale='US',
+    appPackage='com.mytona.seekersnotes.android',
+    appActivity='.GameStartActivity'
+)
+
+appium_server_url = 'http://localhost:4723'
+
+
+class TestAppium(unittest.TestCase):
+    driver = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.driver = webdriver.Remote(
+            appium_server_url,
+            options=UiAutomator2Options().load_capabilities(capabilities)
+        )
+
+    # @classmethod
+    # def tearDownClass(cls) -> None:
+    #     if cls.driver:
+    #         cls.driver.quit()
+
+    def test_launch_app(self) -> None:
+        time.sleep(10)
+        # нажатие ПРИНЯТЬ
+        self.driver.tap([(1200, 1000)])
+        time.sleep(25)
+        # нажатие новой игры слева (слева)
+        self.driver.tap([(830, 930)])
+        time.sleep(3)
+        # ввод 12345 и подтверждение
+        self.driver.press_keycode(8)
+        self.driver.press_keycode(9)
+        self.driver.press_keycode(10)
+        self.driver.press_keycode(11)
+        self.driver.press_keycode(12)
+        self.driver.tap([(1200, 430)])
+        time.sleep(5)
+        # нажатие ИГРАТЬ
+        self.driver.tap([(1200, 800)])
+        # скип интро ролика
+        time.sleep(2)
+        self.driver.tap([(1200, 800)])
+        self.driver.tap([(1200, 800)])
+        time.sleep(5)
+
+    def test_pass_tutor(self) -> None:
+        # первое окно с хелен
+        self.driver.tap([(1600, 690)])
+        time.sleep(3)
+        # второе окно с хелен
+        self.driver.tap([(1600, 690)])
+        time.sleep(3)
+        # первый ХО
+        self.driver.tap([(1620, 730)])
+        time.sleep(3)
+        # первый хинт
+        self.driver.tap([(1800, 1000)])
+        time.sleep(3)
+        # зум ин
+        self.driver.execute_script('mobile: pinchOpenGesture', {
+            'left': 300,
+            'top': 300,
+            'width': 300,
+            'height': 300,
+            'percent': 0.5
+        })
+        time.sleep(0.5)
+        self.driver.execute_script('mobile: pinchCloseGesture', {
+            'left': 300,
+            'top': 300,
+            'width': 300 + 200,
+            'height': 300 + 200,
+            'percent': 1
+        })
+        # подбор хо
+        self.driver.tap([(1200, 820)])
+        time.sleep(3)
+        # зум аут
+        self.driver.execute_script('mobile: pinchOpenGesture', {
+            'left': 300,
+            'top': 300,
+            'width': 500,
+            'height': 500,
+            'percent': 0.5
+        })
+        time.sleep(0.5)
+        self.driver.execute_script('mobile: pinchCloseGesture', {
+            'left': 300,
+            'top': 300,
+            'width': 500 - 200,
+            'height': 500 - 200,
+            'percent': 1
+        })
+        time.sleep(2)
+        # финиш тутора и подбор ласт хо
+        self.driver.tap([(1200, 800)])
+        time.sleep(2)
+        self.driver.tap([(640, 580)])
+        time.sleep(2)
+        self.driver.tap([(1200, 960)])
+        time.sleep(2)
+
+
+if __name__ == '__main__':
+    unittest.main()
